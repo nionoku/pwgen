b24cf4222b5c816db777a1b9612181533ab78a99
Alexander <kondrev@trinity.local>
2019-06-20 17:06:57 +0300

    Raw layouting

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,18 @@
 const app = new Vue({
     el: "#app",
     data: {
-        test: "Hello world"
+        test: "Hello world",
+        address: '',
+        login: '',
+        salt: '',
+        generated: 'Your password here',
+        hasHidden: true
     },
     computed: {
         
     }
 })
+
+function copyToClipboard() {
+    console.log("Copied!")
+}

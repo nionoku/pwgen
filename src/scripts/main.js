643d1e4b1f9dd5dfbe31339cbc286e1eff65f7e6
Alexander <kondrev@trinity.local>
2019-06-21 19:17:13 +0300

    Added toast notification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,5 @@
 const app = new Vue({
-    el: "#app",
+    el: '#app',
     data() {
         return {
             address: '',
@@ -12,12 +12,23 @@ const app = new Vue({
     methods: {
         generatePassword() {
             // TODO реализовать функцию генерирования пароля
-            // this.generated = [this.address, this.login, this.salt].join(":")
+            // this.generated = [this.address, this.login, this.salt].join(':')
         }
     }
 })
 
+function showToast(message) {
+    const toast = document.querySelector('.toast')
+    toast.innerHTML = message
+    toast.className = toast.className + ' show'
+    
+    setTimeout(() => {
+        toast.className = toast.className.replace(' show', '')
+        toast.innerHTML = ''
+    }, 2800)
+}
+
 function copyToClipboard() {
     // TODO реализовать функцию копирования в буфер обмена
-    // console.log("Copied!")
+    // console.log('Copied!')
 }

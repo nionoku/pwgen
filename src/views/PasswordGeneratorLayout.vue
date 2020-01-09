d189a6b6090bf65603ef7215f2e2f2eeb022430c
Alexander <kondrev@trinity.local>
2020-01-09 17:47:32 +0300

    Add template of password generator layout

---
+++ src/views/PasswordGeneratorLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <Toast ref="bottomToast" />
+</template>
+
+<style lang="scss">
+html, body, #app {
+  height: 100%;
+}
+</style>
+<style lang="scss" scoped>
+
+</style>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Toast from '@/components/Toast.vue';
+
+@Component({
+  components: {
+    Toast
+  }
+})
+export default class PasswordGeneratorLayout extends Vue {
+  mounted() {
+    this.showToast();
+  }
+  public showToast() {
+    (this.$refs['bottomToast'] as Toast).show('text');
+  }
+}
+</script>
